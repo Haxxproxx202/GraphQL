05dcae5a83fc5517e9e16ff83b9eb1b943aad839
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-20 20:06:18 +0100

    New employee and customer models added in tandem with their new corresponding managers

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class User(AbstractUser):
@@ -18,4 +18,38 @@ class User(AbstractUser):
             return super().save(*args, **kwargs)
 
 
+class EmployeeManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.EMPLOYEE)
+
+
+class Employee(User):
+    base_role = User.Role.EMPLOYEE
+
+    employee = EmployeeManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for employees"
+
+
+class CustomerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.CUSTOMER)
+
+
+class Customer(User):
+    base_role = User.Role.CUSTOMER
+
+    customer = CustomerManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for customers"
 
